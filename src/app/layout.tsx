fd6c833b0a256925a67ba2eaae4692630662306e
Chandu Gangola <chandugangola444@gmail.com>
2025-08-15 12:02:48 +0530

    added signin/signup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { NuqsAdapter } from "nuqs/adapters/next";
+import { Toaster } from "@/components/ui/sonner";
+
 
 const inter = Inter({
   subsets: ["latin"],
@@ -18,12 +21,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <NuqsAdapter>
+      <html lang="en">
+        <body
+          className={`${inter.className} antialiased`}
+        >
+          <Toaster />
+          {children}
+        </body>
+      </html>
+    </NuqsAdapter>
   );
 }
